073113f9de28f18f62ffa42168ece820fe2abfe3
Piyush Kumar <90065583+piyushk07@users.noreply.github.com>
2023-09-06 10:04:40 +0530

    Update ElectricityBillingSystem.java

--- ElectricityBillingSystem.java
+++ ElectricityBillingSystem.java
@@ -4,10 +4,15 @@ import java.util.*;
 class Customer {
     private String name;
     private int unitsConsumed;
+    private int customerId;
+    private String customerAddress;
 
-    public Customer(String name, int unitsConsumed) {
+    public Customer(String name, int unitsConsumed, int customerId,
+            String customerAddress) {
         this.name = name;
         this.unitsConsumed = unitsConsumed;
+        this.customerId = customerId;
+        this.customerAddress = customerAddress;
     }
 
     public String getName() {
@@ -18,24 +23,30 @@ class Customer {
         return unitsConsumed;
     }
 
+    public String getcustomerAddress() {
+        return customerAddress;
+    }
+
+    public int getcustomerId() {
+        return customerId;
+    }
+
 }
 
-//calculate bill based on unit consumed
+// calculate bill based on unit consumed
 class BillCalculator {
 
     public static double calculateBill(int unitsConsumed) {
-    if(unitsConsumed <= 200){
-        double RATE_PER_UNIT = 4.0;
-        return unitsConsumed * RATE_PER_UNIT;
-    }
-    else if(unitsConsumed > 200 && unitsConsumed < 450){
-        double RATE_PER_UNIT = 6.0;
-        return unitsConsumed * RATE_PER_UNIT;
-    }
-    else{
-        double RATE_PER_UNIT = 8.0;
-        return unitsConsumed * RATE_PER_UNIT;
-    }
+        if (unitsConsumed <= 200) {
+            double RATE_PER_UNIT = 4.0;
+            return unitsConsumed * RATE_PER_UNIT;
+        } else if (unitsConsumed > 200 && unitsConsumed < 450) {
+            double RATE_PER_UNIT = 6.0;
+            return unitsConsumed * RATE_PER_UNIT;
+        } else {
+            double RATE_PER_UNIT = 8.0;
+            return unitsConsumed * RATE_PER_UNIT;
+        }
     }
 }
 
@@ -45,22 +56,31 @@ public class ElectricityBillingSystem {
         System.out.println("Welcome to the Electricity Billing System");
 
         // Customer registration
+        System.out.print("Enter Customer Id: ");
+        int customerId = scanner.nextInt();
         System.out.print("Enter customer name: ");
         String customerName = scanner.nextLine();
+        String cName = scanner.nextLine();
+        System.out.print("Enter Customer address: ");
+        String customerAddress = scanner.nextLine();
         System.out.print("Enter units consumed: ");
         int unitsConsumed = scanner.nextInt();
 
         // Create a customer object
-        Customer cust = new Customer(customerName, unitsConsumed);
+        Customer cust = new Customer(cName, unitsConsumed, customerId,
+                customerAddress);
 
         // Calculate the bill
         double billAmount = BillCalculator.calculateBill(cust.getUnitsConsumed());
 
         // Display the bill
-        System.out.println("\nCustomer Name: " + cust.getName());
-        System.out.println("Units Consumed: " + cust.getUnitsConsumed());
-        System.out.println("Bill Amount: Rs." + billAmount);
-
+        System.out.println("-----------------------------------------");
+        System.out.println("| Customer Id: " + cust.getcustomerId() + " |");
+        System.out.println("| Customer Name: " + cust.getName() + " |");
+        System.out.println("| Customer address: " + cust.getcustomerAddress() + " |");
+        System.out.println("| Units Consumed: " + cust.getUnitsConsumed() + " |");
+        System.out.println("| Bill Amount: Rs." + billAmount + " |");
+        System.out.println("-----------------------------------------");
         scanner.close();
     }
 }
